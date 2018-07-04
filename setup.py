8cc364dd5823739429ed09a900e3f3ff1538c3c5
chengkang <1412950785@qq.com>
2018-07-04 10:16:04 +0800

    add pack support

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import re
+
+try:
+    # Use setuptools if available
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# Check python version info, we need Function Annotations support.
+# https://www.python.org/dev/peps/pep-3107/
+if sys.version_info < (3, 0, 0):
+    raise Exception("PyCLI only support Python 3.0.0+")
+
+
+version = re.compile(r'__version__\s*=\s*"(.*?)"')
+
+
+def get_package_version():
+    """return package version without importing it"""
+    base = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(base, "pycli", "__init__.py"),
+              mode="rt",
+              encoding="utf-8") as initf:
+        for line in initf:
+            m = version.match(line.strip())
+            if not m:
+                continue
+            return m.groups()[0]
+
+
+if __name__ == "__main__":
+    setup(
+        name="pycli",
+        version=get_package_version(),
+        packages=["pycli", "pycli.tests"],
+        description="A Tiny Python CLI Library Based On argparse",
+        author="garenchan",
+        author_email="1412950785@qq.com",
+        url="https://github.com/garenchan/pycli",
+        license="BSD",
+        classifiers=[
+            "License :: OSI Approved :: BSD License",
+            "Programming Language:: Python:: 3",
+            "Programming Language:: Python:: 3.3",
+            "Programming Language:: Python:: 3.4",
+            "Programming Language:: Python:: 3.5",
+            "Programming Language:: Python:: 3.6",
+            "Programming Language:: Python:: Implementation:: CPython",
+            "Programming Language:: Python:: Implementation:: PyPy",
+            "Operating System :: OS Independent",
+        ],
+    )
+
+
+
+
